704716955cbc4ef820f3db0871726faae2c5646c
Carlos León <mail@carlosleon.info>
2016-04-11 17:20:02 +0200

    Update Terraform configuration.
    * Added master and agent machines to the list of resources.
    * Try to open SSH connection and run a dummy shell command to hold on
      the provisioning with Ansible until we know that the servers are
      actually up & running.
    * (Re)named machines with "dcos-" prefix.

--- triton.tf
+++ triton.tf
@@ -4,19 +4,69 @@ provider "triton" {
   key_material = "${file(var.key_path)}"
 }
 
-resource "triton_machine" "bootstrap" {
+resource "triton_machine" "dcos-bootstrap" {
   count   = 1
-  name    = "bootstrap"
+  name    = "dcos-bootstrap"
   image   = "d8e65ea2-1f3e-11e5-8557-6b43e0a88b38"
   package = "g3-standard-1-kvm"
+
   tags {
     role = "bootstrap"
   }
+
+  connection {
+    host = "${triton_machine.dcos-bootstrap.primaryip}"
+    private_key = "${file(var.key_path)}"
+  }
+
+  provisioner "remote-exec" {
+    inline = ["/bin/true"]
+  }
 }
 
-resource "null_resource" "bootstrap-provisioning" {
-  depends_on = ["triton_machine.bootstrap"]
+resource "triton_machine" "dcos-master" {
+  count   = 1
+  name    = "${format(\"dcos-master-%03d\", count.index + 1)}"
+  image   = "d8e65ea2-1f3e-11e5-8557-6b43e0a88b38"
+  package = "g3-standard-1-kvm"
+
+  tags {
+    role = "master"
+  }
+
+  connection {
+    host = "${triton_machine.dcos-bootstrap.primaryip}"
+    private_key = "${file(var.key_path)}"
+  }
+
+  provisioner "remote-exec" {
+    inline = ["/bin/true"]
+  }
+}
+
+resource "triton_machine" "dcos-agent" {
+  count   = 1
+  name    = "${format(\"dcos-agent-%03d\", count.index + 1)}"
+  image   = "d8e65ea2-1f3e-11e5-8557-6b43e0a88b38"
+  package = "g3-standard-1-kvm"
+
+  tags {
+    role = "agent"
+  }
+
+  connection {
+    host = "${triton_machine.dcos-bootstrap.primaryip}"
+    private_key = "${file(var.key_path)}"
+  }
+
+  provisioner "remote-exec" {
+    inline = ["/bin/true"]
+  }
+}
+
+resource "null_resource" "init" {
+  depends_on = ["triton_machine.dcos-bootstrap", "triton_machine.dcos-master", "triton_machine.dcos-agent"]
   provisioner "local-exec" {
-    command = "ansible-playbook plays/triton.yml -u root -i terraform.py"
+    command = "ansible-playbook plays/init.yml -u root -i terraform.py"
   }
 }
