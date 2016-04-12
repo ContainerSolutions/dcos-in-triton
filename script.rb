53977c73ee375c62db711e972929a79a2a8da4e8
muemich <michaelmuellerrw@gmail.com>
2016-04-12 17:39:50 +0200

    hardening

--- script.rb
+++ script.rb
@@ -3,26 +3,33 @@ tf_file = ARGV[0]
 
 tf = JSON.load(File.open(tf_file))
 raise 'no modules in input file' if !tf['modules'] || tf['modules'].empty?
+IP_PATTERN = /^10\./
 
 ips = {'bootstrap' => [], 'master' => [], 'agent' => []}
 tf['modules'][0]['resources'].each do |resource_name, resource|
   if resource_name =~ /\-(agent|master|bootstrap)$/
-    ip_index = resource['primary']['attributes']['ips.#'].to_i - 1
-    raise "#{resource_name} has no IPs!" if ip_index < 0
-    ips[$1] << resource['primary']['attributes']["ips.#{ip_index}"]
+    resource_key = $1
+    ip_count = resource['primary']['attributes']['ips.#'].to_i
+    ip_count.times do |i|
+      ip = resource['primary']['attributes']["ips.#{i}"]
+      if ip =~ IP_PATTERN
+        ips[resource_key] << ip
+        break
+      end
+    end
   end
 end
 
 output = ips.merge({
-'_meta' => {
-  'hostvars' => {
-    ips['master'][0] => {
-      'ansible_host' => ips['master'][0],
-      'ansible_port' => '22',
-      'ansible_user' => 'root',
-      'bootstrap_ip' => ips['bootstrap'][0]
+  '_meta' => {
+    'hostvars' => {
+      ips['master'][0] => {
+        'ansible_host' => ips['master'][0],
+        'ansible_port' => '22',
+        'ansible_user' => 'root',
+        'bootstrap_ip' => ips['bootstrap'][0]
+      }
     }
   }
-}
 })
 puts JSON.pretty_generate(output)
